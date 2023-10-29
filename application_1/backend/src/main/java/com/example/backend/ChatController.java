6fe3edadc1b79ac4e21d16d61a1da9a27cad9f4b
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2023-10-28 22:21:38 -0700

    chat app

---
+++ application_1/backend/src/main/java/com/example/backend/ChatController.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+import com.example.backend.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+@Controller
+public class ChatController {
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public Message sendMessage(Message message) {
+        return message;
+    }
+}
